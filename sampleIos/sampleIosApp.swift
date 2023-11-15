f86348a4c6f144a3bcec5d8f3b8c847f3a1355f3
sooyoung <jtndudz3@gmail.com>
2023-11-15 22:43:22 +0900

    Initial Commit

---
+++ sampleIos/sampleIosApp.swift
@@ -0,0 +1,17 @@
+//
+//  sampleIosApp.swift
+//  sampleIos
+//
+//  Created by blackCrow on 2023/11/15.
+//
+
+import SwiftUI
+
+@main
+struct sampleIosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
